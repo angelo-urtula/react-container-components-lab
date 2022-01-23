a8ce1ef6d04894ecb73bab69128d2acfec5fbd62
angelo <aurtula1@gmail.com>
2022-01-23 17:41:50 -0500

    commit tree bff80d3a06adeae1b3dffa52edbc3cf957c85227

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: "",
+        searchTerm: ""
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        fetch("https://api.nytimes.com/svc/movies/v2/reviews/search.json?",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state.searchTerm)
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+        console.log(this.state.searchTerm)
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" id="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}/>
+
+            </form>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
